7c32430bd336eda4bae90ef535a5262ae285b964
washizu <mokamikan1027@gmail.com>
2024-10-02 18:00:02 +0900

    abc365 c 二分探索

---
+++ ABS/abc365/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using P = pair<int, int>;
+using ll = long long;
+
+int main() {
+  int n;
+  ll m;
+  cin >> n >> m;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+
+  auto f = [&](int x) -> bool {
+    ll s = 0;
+    rep(i, n) s += min(x, a[i]);
+    return s <= m;
+  };
+
+  const int INF = 1001001001;
+  if (f(INF))
+    cout << "infinite" << endl;
+  else {
+    int ac = 0, wa = INF;
+    while (ac + 1 < wa) {
+      int wj = (ac + wa) / 2;
+      if (f(wj))
+        ac = wj;
+      else
+        wa = wj;
+    }
+
+    cout << ac << endl;
+  }
+}
