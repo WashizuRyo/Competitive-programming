a0ea9eb499de589cc5755efaf936d212467858f8
Ryo Washizu <wasizuryo@gmail.com>
2025-09-15 12:32:34 +0900

    abc423 a

--- ABS/abc423/a.js
+++ ABS/abc423/a.js
@@ -2,15 +2,9 @@ const fs = require("fs");
 const lines = fs.readFileSync(0, "utf8").trim().split("\n");
 
 function main() {
-  const x = lines[0]
-  const c = lines[1]
- 
-  for (let i = 0; i < 10000; i++) {
-    const a = x - i * 1000;
-    if (a + a * c / 1000 <= x) {
-      console.log(a);
-    }
-  } 
+  const [x, c] = lines[0].split(" ").map(Number);
+	const k = Math.floor(x / (c + 1000));
+	console.log(k * 1000);
 }
 
 main();
