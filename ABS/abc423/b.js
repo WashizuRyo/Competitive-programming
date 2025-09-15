17537fdeb0da4377d970e83be15f6cf8c92bfdcf
Ryo Washizu <wasizuryo@gmail.com>
2025-09-15 12:16:35 +0900

    abc423 b

--- ABS/abc423/b.js
+++ ABS/abc423/b.js
@@ -3,29 +3,29 @@ const lines = fs.readFileSync(0, "utf8").trim().split("\n");
 
 function main() {
 	const n = Number(lines[0]);
-  const l = lines[1].split(" ").map(Number)
+	const l = lines[1].split(" ").map(Number);
 
-  const visited = new Array(n).fill(false);
+	const visited = new Set();
+	visited.add(0);
+	visited.add(n);
 
-  for (let i = 0; i < n; i++) {
-    if (l[i] == 1) {
-      visited[i] = true;
-    } else {
-      break;
-    }
-  }
+	for (let i = 0; i < n; i++) {
+		if (l[i] === 0) {
+			visited.add(i + 1);
+		} else {
+			break;
+		}
+	}
 
-  for (let i = n - 1; i >= 0; i--) {
-    if (l[i] == 1) {
-      visited[i] = true;
-    } else {
-      break;
-    }
-  }
+	for (let i = n - 1; i >= 0; i--) {
+		if (l[i] === 0) {
+			visited.add(i);
+		} else {
+			break;
+		}
+	}
 
-  let count = visited.filter(Boolean).length + 2;
-  console.log(count);
-  console.log(n + 1 - count);
+	console.log(n + 1 - visited.size);
 }
 
 main();
